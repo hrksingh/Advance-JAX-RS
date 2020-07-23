a091e9dc3e3648f66bd1f83930e23694324c20a9
hrksingh <hrksingh17@gmail.com>
2020-07-23 20:36:32 +0530

    Custom Messagebody Writer example

---
+++ src/main/java/atrium/rest/MessageBodyReaderAndWriter.java
@@ -0,0 +1,22 @@
+package atrium.rest;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/date")
+public class MessageBodyReaderAndWriter {
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public Date getDate() {
+		
+		return Calendar.getInstance().getTime();
+	}
+
+}
