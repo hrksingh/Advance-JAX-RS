fd88178931039339a82e70f5f204ffd41afff5af
hrksingh <hrksingh@gmail.com>
2020-07-23 18:42:47 +0530

    @ApplicationPath Added

---
+++ src/main/java/atrium/rest/AppController.java
@@ -0,0 +1,23 @@
+package atrium.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/test")
+public class AppController {
+
+	@GET
+
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response sayHello() {
+
+		String str = "Hope its works";
+		;
+
+		return Response.status(200).entity(str).build();
+	}
+
+}
